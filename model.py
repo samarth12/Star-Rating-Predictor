05109e6038d44313e36e529f22f782c40f937e3f
samarth12 <samarth12@live.com>
2018-08-09 17:58:02 -0700

    Script to run any classifier

---
+++ model.py
@@ -0,0 +1,21 @@
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
+import csv
+
+import process
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid):
+
+    classifier.fit(feature_vector_train, label)
+
+    predictions = classifier.predict(feature_vector_valid)
+
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+
+    return metrics.accuracy_score(predictions, valid_y)
